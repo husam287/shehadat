20cae2d0f7686b597bece320b2aca7f1837d3f35
Hossam Sherif <husam287@hotmail.com>
2021-08-09 01:23:40 +0200

    fix money of profit

---
+++ android/app/src/main/java/com/shehadat/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shehadat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shehadat',
+  appName: 'shehadat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
