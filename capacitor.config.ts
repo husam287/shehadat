8e1c5126b219bd0dcc0dbb452cba8c1c80dba418
Hossam Sherif <husam287@hotmail.com>
2021-08-09 18:47:34 +0200

    change splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.shehadat',
   appName: 'shehadat',
   webDir: 'www',
-  bundledWebRuntime: false,
+  bundledWebRuntime: false
 };
 
 export default config;
